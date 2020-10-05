01694a33de9786357f73d878fb31f45b788c4614
Livanenkova <arina909@yandex.ru>
2020-10-06 01:03:26 +0300

    add module

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-import Main from '../main/main';
+import Main from "../main/main";
+import SignIn from "../sign-in/sign-in";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
 
 const App = (props) => {
   const {title, genre, year} = props;
 
-  return <Main title={title} genre={genre} year={year} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main title={title} genre={genre} year={year} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
